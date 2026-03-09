c3cf0c0ac0a341a48730a34a1ead4597016c8086
René Siemer <git@renao.de>
2026-03-10 00:25:29 +0100

    Remove temporary build directory after build

--- build-plugin.sh
+++ build-plugin.sh
@@ -1,7 +1,8 @@
 npm install
 npm run build
-composer dump-autoload
 composer install
+composer update
+composer dump-autoload
 sed -i 's/echo \$err;/echo esc_html(\$err);/' build/lib/autoload.php
 
 rm release-plugin-directory/trunk/sams-integration.php
@@ -10,3 +11,4 @@ rm -r release-plugin-directory/trunk/build
 cp sams-integration.php release-plugin-directory/trunk
 cp -r build release-plugin-directory/trunk
 
+rm -rf ./build/
