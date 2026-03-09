2c328dad0591a88bd5de9c79783c663e56530ec5
René Siemer <git@renao.de>
2026-03-10 00:26:00 +0100

    Update RestClient generation

--- generate-restclients.sh
+++ generate-restclients.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-api_spec_file="api-specs/wvv.sams-server.de/2.1.json"
+$generated_client_path = "_Generated/RestClient"
 
+echo "Remove old version from {$generated_client_path}"
+
+rm -rf $generated_client_path
+
+api_spec_file="api-specs/wvv.sams-server.de/2.1.json"
 
 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli generate \
   -i /local/${api_spec_file} \
   -g php \
-  -o /local/src/php/_Generated/RestClient \
+  -o /local/_Generated/RestClient \
   --additional-properties=artifactVersion=2.1.0 \
-  --additional-properties=packageName=SAMSPlugin\\RestClient \
-  --additional-properties=invokerPackage=SAMSPlugin\\RestClient \
-  --additional-properties=composerPackageName=renao/sams-integration-restclient
+  --additional-properties=packageName=SAMS \
+  --additional-properties=invokerPackage=SAMS\\RestClient \
+  --additional-properties=composerPackageName=renao/sams-integration-restclient \
+  --global-property=apiDocs=false \
+  --global-property=modelDocs=false
