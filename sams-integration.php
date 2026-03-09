b2f69bc266275e5c6219cb75acdb7a6f8ea293fc
René Siemer <git@renao.de>
2026-03-10 00:26:53 +0100

    Bump to version 2.0.0

--- sams-integration.php
+++ sams-integration.php
@@ -4,7 +4,7 @@
  * Description:       Displays fixtures and rankings from a SAMS results system
  * Requires at least: 5.6
  * Requires PHP:      7.2
- * Version:           1.2.0
+ * Version:           2.0.0
  * Author:            René Siemer
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
